e1c7857954d496926add131ab0c463df269cdc6a
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2022-10-18 19:25:03 +0500

    Update App.js

--- learningreactnativedaily/App.js
+++ learningreactnativedaily/App.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import DynamicDependentDropdown from './Screens/Screen1/DynamicDependentDropdown'
 
 const App = () => {
   return (
     <>
-      <DynamicDependentDropdown/>
+      <DynamicDependentDropdown />
     </>
   )
 }
