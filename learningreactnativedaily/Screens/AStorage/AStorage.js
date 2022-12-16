539b6a6b8d1bb72c9351354c09d2c82c4d0fb18e
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2022-12-16 22:05:40 +0500

    Async Storage Learning with not immediate state changes issue
    1. state update nhi ho rhi thi immediately tou phr isko useEffect ma daal kr re-render krky update kiya gya or krty bi aysy hi hain ( RECOMMENDED WAY ).
    
    2. Khair now going to check the issue of interstitial ads and then will implement the async storage func in both sound and mail apps

--- learningreactnativedaily/Screens/AStorage/AStorage.js
+++ learningreactnativedaily/Screens/AStorage/AStorage.js
@@ -1,36 +1,78 @@
-import { Alert, Button, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
 import { STYLES } from './AStorageStyles'
-import { useDeviceOrientation } from '@react-native-community/hooks'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { ITEMS } from '../../Constants/AsyncItems'
-
+import React, { useEffect, useState } from 'react'
+import { Alert, Button, Text, View } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useDeviceOrientation } from '@react-native-community/hooks'
 
 const AStorage = () => {
 
-    const [Value, setValue] = useState('')
+    const [Myobjs, setMyobjs] = useState({})
+
+    const generateValues = () => {
+        let time = new Date().getTime()
+        setMyobjs({
+            email: 'Custom.com',
+            timeGenerated: time
+        })
+    }
 
     useEffect(() => {
       getStorageItems()
     }, [])
-    
-    console.log("value " +Value)    
+
+    useEffect(() => {
+        setStorageItems()
+    }, [Myobjs])
+
+    const setStorageItems = async () => {
+
+        const temp =  {...Myobjs}
+
+        console.log(`Data ${JSON.stringify(temp)}`)
+        try {
+            await AsyncStorage.setItem(ITEMS.TEXT, JSON.stringify(temp))
+        } catch (error) {
+            Alert.alert("error while setitems")
+        }
+    }
 
     const getStorageItems = async () => {
         try {
             const data = await AsyncStorage.getItem(ITEMS.TEXT)
-            data == null ? setValue("Nalla") : setValue(data)
-
+            let temp = JSON.parse(data)
+            // console.log(temp)
+            if (temp != null) {
+                const currentTime = new Date().getTime()
+                // console.log("currentTime " + currentTime)
+                if (currentTime > (temp.timeGenerated + 60000)) {
+                    setMyobjs({
+                        email: 'new mail',
+                        timeGenerated: 1111111
+                    })
+                } else {
+                    setMyobjs(temp)
+                }
+            } else {
+                setMyobjs({ email: "Async Is Null", timeGenerated: 0 })
+            }
         } catch (error) {
             console.log(error)
         }
     }
 
-    const setStorageItems = async () => {
+    const check = async () => {
+        let data = await AsyncStorage.getItem(ITEMS.TEXT)
+        let parsedData = JSON.parse(data)
+        alert(parsedData.email + " " + parsedData.timeGenerated)
+    }
+
+    const clearStorage = async () => {
         try {
-            await AsyncStorage.setItem(ITEMS.TEXT, 'HELLO World')
+            await AsyncStorage.clear()
+            getStorageItems()
         } catch (error) {
-            Alert.alert("error while setitems")
+            console.log("error while clear")
         }
     }
 
@@ -42,18 +84,25 @@ const AStorage = () => {
 
                 <View style={STYLES.wrapper(landscape)} >
                     <View style={STYLES.upperBucket(landscape)} >
-                        <Text>{Value}</Text>
+                        <Button title='Clear Khandaq' onPress={() => {
+                            clearStorage()
+                        }} />
+                        <Button title='Get Khandaq' onPress={() => {
+                            getStorageItems()
+                        }} />
+                        <Button title='Check' onPress={() => {
+                            check()
+                        }} />
                     </View>
                     <View style={STYLES.midBucket(landscape)} >
-                        <Text>MidBucket</Text>
-                        <Button title='Butto ' onPress={() => {
-                            setStorageItems()
-                            getStorageItems()
+                        <Text>{Myobjs.email + ' ' + Myobjs.timeGenerated}</Text>
+                        <Button title='Generate with Khandaq' onPress={() => {
+                            generateValues()
                         }} />
                     </View>
                 </View>
                 <View style={STYLES.lowerBucket(landscape)} >
-                    <Text  >Lower Bucket</Text>
+                    <Text>lowerBucket</Text>
                 </View>
             </View>
 
